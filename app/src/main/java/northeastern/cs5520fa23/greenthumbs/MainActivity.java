fbfa2c792cfc35f445143725ff51d9dd4b110440
Karthik J Bharadwaj <37576167+Karthik1235@users.noreply.github.com>
2023-11-22 17:13:16 -0500

    Using Shared Preferences for home location

--- app/src/main/java/northeastern/cs5520fa23/greenthumbs/MainActivity.java
+++ app/src/main/java/northeastern/cs5520fa23/greenthumbs/MainActivity.java
@@ -1,26 +1,74 @@
 package northeastern.cs5520fa23.greenthumbs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import northeastern.cs5520fa23.greenthumbs.services.WeatherService;
 
 public class MainActivity extends AppCompatActivity {
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startWeatherService();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                startWeatherService();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            @Override
+            public void onProviderEnabled(String provider) {}
+
+            @Override
+            public void onProviderDisabled(String provider) {}
+
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
     }
 
     private void startWeatherService() {
-        Intent serviceIntent = new Intent(this, WeatherService.class);
-        // Update with home location fetched from user
-        serviceIntent.putExtra(WeatherService.latitude, 42.3458);
-        serviceIntent.putExtra(WeatherService.longitude, -71.0947);
-        startService(serviceIntent);
+        SharedPreferences sharedPreferences = getSharedPreferences("AppPreferences", MODE_PRIVATE);
+        float latitude = sharedPreferences.getFloat("HomeLatitude", 0);
+        float longitude = sharedPreferences.getFloat("HomeLongitude", 0);
+
+        if (latitude != 0 && longitude != 0) {
+            Intent serviceIntent = new Intent(this, WeatherService.class);
+            serviceIntent.putExtra(WeatherService.latitude, latitude);
+            serviceIntent.putExtra(WeatherService.longitude, longitude);
+            startService(serviceIntent);
+        } else {
+            // Ask user to set home location
+        }
     }
+
+    public void saveHomeLocation(double latitude, double longitude) {
+        SharedPreferences sharedPreferences = getSharedPreferences("AppPreferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat("HomeLatitude", (float) latitude);
+        editor.putFloat("HomeLongitude", (float) longitude);
+        editor.apply();
+    }
+
 }
