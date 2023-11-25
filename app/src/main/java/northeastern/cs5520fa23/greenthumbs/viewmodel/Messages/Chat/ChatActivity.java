5cc9be328f326886f842005c5a443655bdbbfcc9
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2023-11-25 17:46:08 -0500

    update chat layout and logic

--- app/src/main/java/northeastern/cs5520fa23/greenthumbs/viewmodel/Messages/Chat/ChatActivity.java
+++ app/src/main/java/northeastern/cs5520fa23/greenthumbs/viewmodel/Messages/Chat/ChatActivity.java
@@ -1,10 +1,12 @@
 package northeastern.cs5520fa23.greenthumbs.viewmodel.Messages.Chat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import northeastern.cs5520fa23.greenthumbs.R;
@@ -21,5 +23,12 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        msgList = new ArrayList<>();
+        msgRecyclerView = findViewById(R.id.chat_recycler_view);
+        msgRecyclerView.setHasFixedSize(true);
+        msgRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        msgAdapter = new MessageAdapter(msgList, this);
+        msgRecyclerView.setAdapter(msgAdapter);
     }
 }
