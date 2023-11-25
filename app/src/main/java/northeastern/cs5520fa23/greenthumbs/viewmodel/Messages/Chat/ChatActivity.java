51f7e4d543f59a05d69215d670c835697801d4b0
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2023-11-25 16:10:16 -0500

    add logic and update layouts for chat

--- app/src/main/java/northeastern/cs5520fa23/greenthumbs/viewmodel/Messages/Chat/ChatActivity.java
+++ app/src/main/java/northeastern/cs5520fa23/greenthumbs/viewmodel/Messages/Chat/ChatActivity.java
@@ -1,12 +1,21 @@
 package northeastern.cs5520fa23.greenthumbs.viewmodel.Messages.Chat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.List;
 
 import northeastern.cs5520fa23.greenthumbs.R;
 
 public class ChatActivity extends AppCompatActivity {
+    private final int SENT_MESSAGE_TYPE = 0;
+    private final int RECEIVED_MESSAGE_TYPE = 1;
+    private RecyclerView msgRecyclerView;
+    private MessageAdapter msgAdapter;
+    private List<ChatMessage> msgList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
