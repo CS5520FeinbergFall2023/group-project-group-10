a1109877fe49e033d237a5d92835aebb86e9eda1
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2023-11-29 23:11:22 -0500

    add image upload to post

--- app/src/main/java/northeastern/cs5520fa23/greenthumbs/viewmodel/SocialFeed/CreatePostDialog.java
+++ app/src/main/java/northeastern/cs5520fa23/greenthumbs/viewmodel/SocialFeed/CreatePostDialog.java
@@ -6,7 +6,12 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
@@ -16,11 +21,46 @@ import northeastern.cs5520fa23.greenthumbs.R;
 public class CreatePostDialog extends DialogFragment {
     Button postButton;
     Button cancelButton;
+    private Button addImgButton;
+    private ImageView postImage;
+    private ActivityResultLauncher<PickVisualMediaRequest> imgSelect;
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         View view = getLayoutInflater().inflate(R.layout.fragment_create_post, null);
+        postButton = view.findViewById(R.id.create_post_button);
+        cancelButton = view.findViewById(R.id.cancel_create_post_button);
+        addImgButton = view.findViewById(R.id.create_post_add_image_button);
+        postImage = view.findViewById(R.id.create_post_image);
+        imgSelect = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+            if (uri != null) {
+                postImage.setImageURI(uri);
+                postImage.setVisibility(View.VISIBLE);
+            } else {
+
+            }
+        });
+        postImage.setVisibility(View.GONE);
+        addImgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getImg();
+            }
+        });
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreatePostDialog.this.getDialog().cancel();
+            }
+        });
         builder.setView(view);
                 /*
                 .setPositiveButton("Post", new DialogInterface.OnClickListener() {
@@ -39,21 +79,24 @@ public class CreatePostDialog extends DialogFragment {
 
                  */
 
-        postButton = view.findViewById(R.id.create_post_button);
-        cancelButton = view.findViewById(R.id.cancel_create_post_button);
-        postButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //
+
+        return builder.create();
+    }
+
+    private void getImg() {
+
+        /*Toast.makeText(getContext(), "HERE", Toast.LENGTH_LONG).show();
+        ActivityResultLauncher<PickVisualMediaRequest> imgSelect = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+            if (uri != null) {
+                postImage.setImageURI(uri);
+                postImage.setVisibility(View.VISIBLE);
+            } else {
+
             }
         });
 
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CreatePostDialog.this.getDialog().cancel();
-            }
-        });
-        return builder.create();
+         */
+
+        imgSelect.launch(new PickVisualMediaRequest.Builder().setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE).build());
     }
 }
