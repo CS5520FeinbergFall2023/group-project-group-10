112a509bae63fdcf6c4747ccdfd463b30cf67691
Karthik J Bharadwaj <37576167+Karthik1235@users.noreply.github.com>
2023-12-05 21:24:28 +0530

    Handled errors during user creation

--- app/src/main/java/northeastern/cs5520fa23/greenthumbs/MainActivity.java
+++ app/src/main/java/northeastern/cs5520fa23/greenthumbs/MainActivity.java
@@ -6,11 +6,8 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import northeastern.cs5520fa23.greenthumbs.model.services.WeatherService;
@@ -18,7 +15,6 @@ import northeastern.cs5520fa23.greenthumbs.viewmodel.Messages.MessageHomeFragmen
 import northeastern.cs5520fa23.greenthumbs.viewmodel.Profile.ProfileFragment;
 import northeastern.cs5520fa23.greenthumbs.viewmodel.SetLocationFragment;
 import android.view.MenuItem;
-import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -26,6 +22,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
 import northeastern.cs5520fa23.greenthumbs.viewmodel.Dashboard.DashboardFragment;
 import northeastern.cs5520fa23.greenthumbs.viewmodel.Garden.GardenFragment;
 import northeastern.cs5520fa23.greenthumbs.viewmodel.Settings.SettingsFragment;
@@ -49,12 +48,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-//        FirebaseUser user = mAuth.getCurrentUser();
-//        if (user == null) {
-//            Intent i = new Intent(MainActivity.this, LogInActivity.class);
-//            startActivity(i);
-//            finish();
-//        }
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            Intent i = new Intent(MainActivity.this, LogInActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 
     /*
@@ -80,19 +79,17 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(MainActivity.this, LogInActivity.class);
             startActivity(i);
             finish();
+            return;
         }
-
         uid = mAuth.getCurrentUser().getUid();
 
-        FirebaseDatabase.getInstance().getReference("users").child(uid).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "Unable to fetch username", Toast.LENGTH_LONG).show();
-                } else {
-                    User currUser = task.getResult().getValue(User.class);
-                    username = currUser.getUsername();
-                }
+        FirebaseDatabase.getInstance().getReference("users").child(uid).get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Toast.makeText(MainActivity.this, "Unable to fetch username", Toast.LENGTH_LONG).show();
+            } else {
+                User currUser = task.getResult().getValue(User.class);
+                assert currUser != null;
+                username = currUser.getUsername();
             }
         });
 
@@ -111,16 +108,13 @@ public class MainActivity extends AppCompatActivity {
         //setSupportActionBar(toolbar);
         toolbar.inflateMenu(R.menu.appbar);
 
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                if (item.getItemId() == R.id.appbar_profile) {
-                    Fragment profileFragment = ProfileFragment.newInstance(username, FirebaseAuth.getInstance().getCurrentUser().getUid());
-                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, profileFragment).commit();
-                    return true;
-                }
-                return false;
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.appbar_profile) {
+                Fragment profileFragment = ProfileFragment.newInstance(username, Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, profileFragment).commit();
+                return true;
             }
+            return false;
         });
         // When app is opened go to dashboard
         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, dashboardFragment).commit();
