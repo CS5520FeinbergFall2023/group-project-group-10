4e3b25b43d5714e9e217b4a32a6f5d257a8d7b89
Raphael Russo <122693743+rrussoneu@users.noreply.github.com>
2023-12-06 19:10:27 -0500

    add garden stat activity

---
+++ app/src/main/java/northeastern/cs5520fa23/greenthumbs/viewmodel/Garden/GardenStats/GardenStatsActivity.java
@@ -0,0 +1,16 @@
+package northeastern.cs5520fa23.greenthumbs.viewmodel.Garden.GardenStats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import northeastern.cs5520fa23.greenthumbs.R;
+
+public class GardenStatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_garden_stats);
+    }
+}
