91c547e839d787b94f3daccdb58393d4ae2ebe11
Karthik J Bharadwaj <37576167+Karthik1235@users.noreply.github.com>
2023-12-09 14:44:31 +0530

    Fixed notification bug with like

--- app/src/main/java/northeastern/cs5520fa23/greenthumbs/model/PostNotificationHelper.java
+++ app/src/main/java/northeastern/cs5520fa23/greenthumbs/model/PostNotificationHelper.java
@@ -6,6 +6,7 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,48 +32,53 @@ public class PostNotificationHelper {
         DatabaseReference friendsRef = FirebaseDatabase.getInstance().getReference("users").child(currentUserId).child("friends");
         DatabaseReference postsRef = FirebaseDatabase.getInstance().getReference("posts");
 
-        friendsRef.addValueEventListener(new ValueEventListener() {
+        friendsRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot friendSnapshot : dataSnapshot.getChildren()) {
                     String friendUserId = friendSnapshot.child("friend_id").getValue(String.class);
 
-                    postsRef.orderByChild("uid").equalTo(friendUserId).addValueEventListener(new ValueEventListener() {
+                    postsRef.orderByChild("uid").equalTo(friendUserId).addChildEventListener(new ChildEventListener() {
                         @Override
-                        public void onDataChange(@NonNull DataSnapshot postSnapshot) {
-                            for (DataSnapshot snapshot : postSnapshot.getChildren()) {
-                                String timestampString = snapshot.child("timestamp").getValue(String.class) + "Z";
-                                Instant instant = Instant.parse(timestampString);
-                                long postTimestamp = instant.toEpochMilli();
-                                String postId = snapshot.getKey();
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, String previousChildName) {
+                            // New post added, handle notification here
+                            String timestampString = snapshot.child("timestamp").getValue(String.class) + "Z";
+                            Instant instant = Instant.parse(timestampString);
+                            long postTimestamp = instant.toEpochMilli();
+                            String postId = snapshot.getKey();
 
-                                if(notifiedPostIds.contains(postId)){
-                                    break;
-                                }
-                                fetchLastActiveTimestamp(currentUserId, new OnTimestampFetchedListener() {
-                                    @Override
-                                    public void onTimestampFetched(long timestamp) {
-                                        if (postTimestamp > timestamp && postTimestamp > getLastNotifiedTimestamp()) {
-                                            String postText = snapshot.child("post_text").getValue(String.class);
-                                            String friendUsername = snapshot.child("username").getValue(String.class);
-                                            String image_uri = snapshot.child("img_uri").getValue(String.class);
-
-                                            // Trigger a notification for the new post
-                                            NotificationHelper.showNotification(
-                                                    context,
-                                                    friendUsername + " made a new post",
-                                                    postText,
-                                                    R.drawable.baseline_notifications_24, image_uri
-                                            );
-                                            setLastNotifiedTimestamp(postTimestamp);
-                                            notifiedPostIds.add(postId);
-                                        }
-                                    }
-                                });
-
+                            if (notifiedPostIds.contains(postId)) {
+                                return;
                             }
+                            fetchLastActiveTimestamp(currentUserId, new OnTimestampFetchedListener() {
+                                @Override
+                                public void onTimestampFetched(long timestamp) {
+                                    if (postTimestamp > timestamp && postTimestamp > getLastNotifiedTimestamp()) {
+                                        String postText = snapshot.child("post_text").getValue(String.class);
+                                        String friendUsername = snapshot.child("username").getValue(String.class);
+                                        String image_uri = snapshot.child("img_uri").getValue(String.class);
+
+                                        // Trigger a notification for the new post
+                                        NotificationHelper.showNotification(
+                                                context,
+                                                friendUsername + " made a new post",
+                                                postText,
+                                                R.drawable.baseline_notifications_24, image_uri
+                                        );
+                                        setLastNotifiedTimestamp(postTimestamp);
+                                        notifiedPostIds.add(postId);
+                                    }
+                                }
+                            });
                         }
 
+                        // Implement other methods of ChildEventListener as needed, possibly empty
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, String previousChildName) {}
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {}
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, String previousChildName) {}
                         @Override
                         public void onCancelled(@NonNull DatabaseError databaseError) {
                             // Handle error
@@ -87,6 +93,7 @@ public class PostNotificationHelper {
             }
         });
     }
+
 
     private long getLastNotifiedTimestamp() {
         return sharedPreferences.getLong("lastNotifiedTimestamp", 0);
